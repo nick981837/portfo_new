10735ecd36040b4fff4dead548419bc203c2db88
MENG-KU CHEN <60210975+nick981837@users.noreply.github.com>
2020-08-25 14:27:05 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,48 @@
+import os
+import csv
+from flask import Flask, render_template, send_from_directory, request, redirect
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('./index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('C:\\Users\\陳孟谷\\PycharmProjects\\webserver\\database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('C:\\Users\\陳孟谷\\PycharmProjects\\webserver\\database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method =='POST':
+        try:
+          data = request.form.to_dict()
+          username = request.form['name']
+          write_to_csv(data)
+          return redirect('/thankyou.html/<username>')
+        except:
+            return 'did not save to database'
+        else:
+          return 'something went wrong and try again'
+
+@app.route('/thankyou.html/<username>')
+def thankyou(username):
+    return render_template('./thankyou.html', name=name)
+
+
+
